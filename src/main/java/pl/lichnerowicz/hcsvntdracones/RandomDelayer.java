1e12d27566cac981b998910deb35721f62f80bdc
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-03-02 11:09:41 +0100

    Add missing docs

--- src/main/java/pl/lichnerowicz/hcsvntdracones/RandomDelayer.java
+++ src/main/java/pl/lichnerowicz/hcsvntdracones/RandomDelayer.java
@@ -1,16 +1,35 @@
-// andrzej lichnerowicz, unlicensed (~public domain)
+/*
+ * andrzej lichnerowicz, unlicensed (~public domain)
+ */
 package pl.lichnerowicz.hcsvntdracones;
 
 import java.security.SecureRandom;
 import cern.jet.random.Poisson;
 
+/**
+ * This class consist of methods for generating random
+ * values, for use with Camel routes in ActiveMQ.
+ *
+ * @author Andrzej Lichnerowicz
+ */
+
 public class RandomDelayer {
+    /**
+     * Takes one integer from Gauss distribution.
+     *
+     * @param max maximum delay, in milliseconds
+     */
     public long getGauss(long max) {
         double random = new SecureRandom().nextGaussian();
         return (long)(random * max);
     }
 
-    public int getPoisson(double lambda) {
-        return Poisson.staticNextInt(lambda);
+    /**
+     * Takes one integer from Poisson distribution
+     *
+     * @param mean average number of delay, in milliseconds
+     */
+    public int getPoisson(double mean) {
+        return Poisson.staticNextInt(mean);
     }
 }
